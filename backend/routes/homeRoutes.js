ad3f11f8564f595f6aed3db7420925b991a5e187
praveen <116637585+praveen-047@users.noreply.github.com>
2025-11-05 09:56:20 +0530

    5-11-25- backend

--- backend/routes/homeRoutes.js
+++ backend/routes/homeRoutes.js
@@ -1,85 +1,85 @@
-import express from 'express';
-import connectDB from '../mysql/db.js';
-import auth from '../middleware/auth.js'
+import express from "express";
+import connectDB from "../mysql/db.js";
+import auth from "../middleware/auth.js";
 
 const router = express.Router();
 
 const db = await connectDB();
 
-router.get('/home',auth, async (req,res)=>{
-    try{
-        const {username,email} = req.user;
-        
+router.get("/home", auth, async (req, res) => {
+  try {
+    const { username, email } = req.user;
 
-        const [shopsList] = await db.execute(`select * from salons`)
-        if (shopsList.length == 0){
-            return res.json({msg:"no data about saloon shops"})
-        }
-        res.status(201).json({msg:"saloon data fetched successfully", shopsList:shopsList})
-    }catch(error){
-        console.log(error);
-        res.status(500).json({msg:"server error"})
-        
+    const [shopsList] = await db.execute(`select * from salons`);
+    if (shopsList.length == 0) {
+      return res.json({ msg: "no data about saloon shops" });
     }
-})
+    res
+      .status(201)
+      .json({ msg: "saloon data fetched successfully", shopsList: shopsList });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ msg: "server error" });
+  }
+});
 
-router.get('/saloon/:salon_id',auth, async(req,res)=>{
-    try {
-        const{salon_id} = req.params;
+router.get("/saloon/:salon_id", auth, async (req, res) => {
+  try {
+    const { salon_id } = req.params;
 
-        const [salonData] = await db.execute(`select * from services where salon_id = ?`,[salon_id]);
-        if (salonData.length == 0){
-            return res.status(400).json({msg:"no data found"})
-        }
-        res.status(201).json({msg:'data successfully fetched', data:salonData});
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({msg:"server error"})
+    const [salonData] = await db.execute(
+      ` select s.name as salon_name, s.image_url as logo, s.address as address,sr.*
+        from salons s inner join services sr
+        on s.salon_id = sr.salon_id
+        where s.salon_id = ?`,
+      [salon_id]
+    );
+    if (salonData.length == 0) {
+      return res.status(400).json({ msg: "no data found" });
     }
-})
+    res.status(201).json({ msg: "data successfully fetched", salonData: salonData });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ msg: "server error" });
+  }
+});
 
-router.get('/bookings',auth, async(req,res)=>{
-    try {
-        const{user_id,username,email} = req.user
+router.get("/bookings", auth, async (req, res) => {
+  try {
+    const { user_id, username, email } = req.user;
 
-        const [userBokings] = await db.execute(
-            `select * 
-            from services
-            where service_id in(select service_id
-                                from appointments
-                                where user_id = ?)`,[user_id]
-        ) 
+    const [bookingsData] = await db.execute(
+      `select s.name as saloon_name, s.image_url as logo, s.address as address, 
+		    a.appointment_date ,a.appointment_time,
+            sr.name as service_name, sr.duration_minutes as duration, sr.price as price
+        from salons s inner join appointments a inner join services sr
+        on s.salon_id = a.salon_id and a.service_id = sr.service_id
+        where a.user_id = ?`,
+      [user_id]
+    );
 
-        const [salonData] = await db.execute(
-            `
-            select *
-            from salons
-            where salon_id in (select salon_id
-                            from appointments
-                            where user_id = ?)`,[user_id]
-        )
+    res
+      .status(201)
+      .json({ msg: "data fetched successfully", bookings: bookingsData });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ msg: "server error" });
+  }
+});
 
-        res.status(201).json({msg:"data fetched successfully",bookings:userBokings, salons:salonData})
-        
-    } catch (error) {
-        console.log(error)
-        res.status(500).json({msg:"server error"})
-    }
-})
+router.put("/update", auth, async (req, res) => {
+  const { user_id } = req.user;
+  const { username, email, mobile } = req.body;
 
-
-router.put('/update',auth, async(req,res)=>{
-    const{user_id} = req.user 
-    const{username,email,mobile} = req.body
-
-    const [data] = await db.execute(`
+  const [data] = await db.execute(
+    `
         update users
         set username=?, email= ?, mobile = ?
-        where user_id = ?`,[username,email,mobile,user_id])
+        where user_id = ?`,
+    [username, email, mobile, user_id]
+  );
 
-    res.status(200).json({msg:'data updated successfully',data:data})
-})
-
-
+  res.status(200).json({ msg: "data updated successfully", data: data });
+});
 
 export default router;
